3431bdd797503be8241aa7c1cb228f6800e96d45
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-05-28 03:13:24 -0400

    base de datos conectada con éxito

--- queries/consultas.js
+++ queries/consultas.js
@@ -1,1 +1,16 @@
 import pool from '../config/db.js';
+
+const getData = async () => {
+    try {
+        const consulta = {
+            text: 'SELECT * FROM canciones',
+        };
+    const response = await pool.query(consulta);
+    return response.rows
+    } catch (error) {
+        console.log(error.code, error.message);
+    };
+};
+
+
+export { getData }
--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 import path from 'path';
 const __dirname = import.meta.dirname;
 import pool from '../config/db.js';
+import { getData } from '../queries/consultas.js'
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'))
@@ -12,5 +13,11 @@ router.get('/date', async (req,res) => {
     const result = await pool.query('select now()');
     res.send(result);
 });
+
+router.get('/canciones', async (req,res) => {
+    const result = await getData();
+    console.log(result);
+    res.json(result);
+})
 
 export default router;
